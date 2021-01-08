71b8fd6ed7bc19d60cbab967b7c06d71b1743d81
marzekan <marko.zekan@gmail.com>
2021-01-08 21:09:52 +0100

    rules file updated. checkWin and isGameOver methods implemented.

--- rules.py
+++ rules.py
@@ -1,53 +1,65 @@
 class Board:
     def __init__(self):
+        '''
+            [0,1,2,3,4,5,6,7,8]
+
+            | 0, 1, 2 |
+            | 3, 4, 5 |
+            | 6, 7, 8 |
+        '''
         self.rows = 3
         self.cols = 3
 
         self.setting = [None for x in range(self.rows * self.cols)]
 
-    def checkCols(self):
+    def checkColsWin(self) -> str:
 
-        for i in range(9):
-            if i in [0, 1, 2]:
-                if self.setting[i] == self.setting[i+3] == self.setting[i+6]:
-                    pass
+        # Check if some column contains same values.
+        for i in [0, 1, 2]:
+            if self.setting[i] == self.setting[i+3] == self.setting[i+6]:
+                if self.setting[i] != None:
+                    return self.setting[i]
+
+    def checkRowsWin(self) -> str:
+
+        # Check if some column contains same values.
+        for i in [0, 3, 6]:
+            if self.setting[i] == self.setting[i+1] == self.setting[i+2]:
+                if self.setting[i] != None:
+                    return self.setting[i]
+
+    def checkDiagonalsWin(self) -> str:
+
+        # Check left diagonal.
+        if self.setting[0] == self.setting[4] == self.setting[8]:
+            return self.setting[0]
+
+        # Check right diagonal.
+        elif self.setting[2] == self.setting[4] == self.setting[6]:
+            return self.setting[2]
 
     # Checks if there is a win on the board.
     def checkWin(self) -> str:
 
-        # Win is in the first row.
-        if len(set(self.setting[0:3])) == 1:
-            return self.setting[0]
-        # Win is in the second row.
-        elif len(set(self.setting[3:6])) == 1:
-            return self.setting[3]
-        # Win is in the third row.
-        elif len(set(self.setting[6:9])) == 1:
-            return self.setting[6]
+        colWin, rowWin, diagonWin = self.checkColsWin(
+        ), self.checkRowsWin(), self.checkDiagonalsWin()
 
-        elif checkCols():
-            return
+        for win in [colWin, rowWin, diagonWin]:
+            if win != None:
+                return win
 
-    def isGameOver(self) -> str:
+    def isGameOver(self) -> bool:
 
-        # Return 'X'
-        if isWinningMove():
-            pass
-        #
+        if self.checkWin() != None:
+            return True
+
         elif None not in self.setting:
             return True
 
+        else:
+            return False
 
-def resetBoard(self):
-    # Reset the board to beggining state.
-    self.setting = [None for x in range(self.rows * self.cols)]
+    def resetBoard(self):
 
-
-board = Board()
-print(board.setting)
-print(len(board.setting))
-
-lis = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-print(lis[0:3])
-print(lis[3:6])
-print(lis[6:9])
+        # Reset the board to beggining state.
+        self.setting = [None for x in range(self.rows * self.cols)]
