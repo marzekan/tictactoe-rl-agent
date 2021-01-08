6d9076f1f0a48ad4e3b47581a46b27c23d31da5c
Luka Čosić <37144180+lucosic@users.noreply.github.com>
2021-01-08 21:56:12 +0100

    Main is checking for game over and when the game is reset players switch sides. File for agent is created along with defined parameters in the constructor.

--- main.py
+++ main.py
@@ -1,85 +1,108 @@
 import tkinter as tk
 
+from rules import Board
+from agent import Agent
 
-setting = [None, None, None, None, None, None, None, None, None]
+# setting = [None, None, None, None, None, None, None, None, None]
 boardButtons = []
 
+
 class GameBoard(tk.Frame):
+
     def __init__(self, root = None):
         tk.Frame.__init__(self, root)
 
         self.master.title("Krizic Kruzic")
         self.master.configure(background='black')
 
-        global playerSign
-        playerSign = "X"
-        global agentSign
-        agentSign = "O"
-        
+        self.board = Board()
+        self.playerSign = "X"
+        self.agentSign = "O"
+        self.agent = Agent(self.board.setting, self.agentSign)
+
         self.createBoard()
 
         self.pack(fill="both")
     
     def createBoard(self):
-        # Create labels
-        self.info_label = tk.Label(self, text = "TIC TAC TOE", font='SegoeUI 30 bold' )
-        self.train_label = tk.Label(self, text = "Agent još nije treniran", font='SegoeUI 10' )
+        # Create Frames
+        self.titleFrame = tk.Frame(self, bg ="black")
+        self.titleFrame.grid(row=0, column=0, columnspan=1, sticky='ew')
+        self.boardFrame = tk.Frame(self, bg ="black")
+        self.boardFrame.grid(row=1, column=0, columnspan=3, sticky='ew')
+
+        # Create and place Title
+        self.titlelabel = tk.Label(self.titleFrame, height=2, width=18, text = "TIC TAC TOE", font='SegoeUI 20 bold', fg="white",bg="black")
+        self.titlelabel.grid(row=0, column=0)
 
         # Create buttons
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(0)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(1)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(2)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(3)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(4)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(5)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(6)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(7)))
-        boardButtons.append(tk.Button(self, height = 4, width = 8, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.setMove(8)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(0)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(1)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(2)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(3)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(4)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(5)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(6)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(7)))
+        boardButtons.append(tk.Button(self.boardFrame, height = 3, width = 6, text=' ', font='SegoeUI 20 bold', bg='black', fg='white', command = lambda : self.playerSetMove(8)))
 
-        self.reset_btn = tk.Button(self, height=2, width=16, text='RESET GAME', font='SegoeUI 10 bold', bg='green', fg='white', command = lambda : self.reset())
-        self.train_btn = tk.Button(self, height=2, width=16, text='TRAIN AGENTS', font='SegoeUI 10 bold', bg='green', fg='white', command = lambda : self.reset())
+        self.reset_btn = tk.Button(self.boardFrame, height=2, width=16, text='RESET GAME', font='SegoeUI 10 bold', bg='green', fg='white', command = lambda : self.reset())
+        self.train_btn = tk.Button(self.boardFrame, height=2, width=16, text='TRAIN AGENT', font='SegoeUI 10 bold', bg='green', fg='white', command = lambda : self.reset())
 
         # Place buttons
-        # self.button0.grid(row = 1, column = 0)
-        # self.button1.grid(row = 1, column = 1)
-        # self.button2.grid(row = 1, column = 2)
-        # self.button3.grid(row = 2, column = 0)
-        # self.button4.grid(row = 2, column = 1)
-        # self.button5.grid(row = 2, column = 2)  
-        # self.button6.grid(row = 3, column = 0)
-        # self.button7.grid(row = 3, column = 1)
-        # self.button8.grid(row = 3, column = 2)
+        boardButtons[0].grid(row = 2, column = 0)
+        boardButtons[1].grid(row = 2, column = 1)
+        boardButtons[2].grid(row = 2, column = 2)
+        boardButtons[3].grid(row = 3, column = 0)
+        boardButtons[4].grid(row = 3, column = 1)
+        boardButtons[5].grid(row = 3, column = 2)  
+        boardButtons[6].grid(row = 4, column = 0)
+        boardButtons[7].grid(row = 4, column = 1)
+        boardButtons[8].grid(row = 4, column = 2)
 
-        boardButtons[0].grid(row = 1, column = 0)
-        boardButtons[1].grid(row = 1, column = 1)
-        boardButtons[2].grid(row = 1, column = 2)
-        boardButtons[3].grid(row = 2, column = 0)
-        boardButtons[4].grid(row = 2, column = 1)
-        boardButtons[5].grid(row = 2, column = 2)  
-        boardButtons[6].grid(row = 3, column = 0)
-        boardButtons[7].grid(row = 3, column = 1)
-        boardButtons[8].grid(row = 3, column = 2)
+        self.reset_btn.grid(row=5, column=0, columnspan=3, sticky='ew')
+        self.train_btn.grid(row=6, column=0, columnspan=3, sticky='ew')
 
-        self.reset_btn.grid(row=4, column=0)
-        self.train_btn.grid(row=4, column=2)
+        # Create and place labels
+        #self.train_label = tk.Label(self, text = "Agent još nije treniran", font='SegoeUI 10' )
 
-    def setMove(self, buttonNumber):
-        setting[buttonNumber] = playerSign
-        boardButtons[buttonNumber].configure(text=playerSign, state=tk.DISABLED)
-        print(setting)
+    def playerSetMove(self, buttonNumber):
+        self.board.setting[buttonNumber] = self.playerSign
+        boardButtons[buttonNumber].configure(text=self.playerSign, bg='blue', fg='red', state=tk.DISABLED)
+        print(self.board.setting, "set move")
+        self.checkGameOver()
 
-    def updateMove(self, setting):
-        self.setting = setting
+    def agentSetMove(self):
+        print("Agent radi potez")
+
+    def updateBoardSetting(self):
         for i in range(9):
-            if setting[i] is None:
-                boardButtons[i].configure(text=" ", state=tk.NORMAL)
+            if self.board.setting[i] is None:
+                boardButtons[i].configure(text=" ", bg='black', fg='white', state=tk.NORMAL)
             else:
-                boardButtons[i].configure(text=setting[i], state=tk.DISABLED)
+                boardButtons[i].configure(text=self.board.setting[i], bg='blue', fg='red', state=tk.DISABLED)
+        self.checkGameOver()
+    
+    def checkGameOver(self):
+        if self.board.isGameOver() is True:
+            print("Igra gotova")
+            winner = self.board.checkWin()
+            print("Pobijedio je: ", winner)
+            for i in range(9):
+                boardButtons[i].configure(state=tk.DISABLED)
+            # ispisi pobjednika
     
     def reset(self):
-        self.setting = [None, None, None, None, None, None, None, None, None]
-        self.updateMove(self.setting)
-        # POSALJI MARKU DA RESETIRA PLOCU 
+        self.board.resetBoard()
+        self.updateBoardSetting()
+        if self.playerSign == "X":
+            self.playerSign = "O"
+            self.agentSign = "X"
+            self.agentSetMove
+        else:
+            self.playerSign = "X"
+            self.agentSign = "O"
+        print(self.board.setting, "reset")
 
 if __name__ == "__main__":
     GameBoard()
