eb4279956a1530d883e145ae5449f17f69b3e988
marzekan <marko.zekan@gmail.com>
2021-01-09 17:25:53 +0100

    save and load function changed in agent.py

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ from brain import QLearning
 
 
 class Agent:
-    def __init__(self, setting, agentSign, strategy='random', loadSaved=False):
+    def __init__(self, setting, agentSign, strategy='random'):
 
         self.states = setting
         self.sign = agentSign
@@ -16,9 +16,6 @@ class Agent:
 
         if strategy == "q":
             self.strategy = QLearning()
-
-        if loadSaved == True:
-            self.loadQStates()
 
         self.actions = self.getAvailablePos()
 
@@ -45,13 +42,13 @@ class Agent:
         print(self.strategy.states)
 
     # Saves Q values to pickle file.
-    def saveQStates(self):
+    def saveQStates(self, file_name):
         Qvalues = self.strategy.states
         with open("trained.pkl", "wb") as file:
             pickle.dump(Qvalues, file)
 
     # Loads Q values from pickle file.
-    def loadQStates(self):
+    def loadQStates(self, file_name):
         with open("trained.pkl", "rb") as file:
             Qvalues = pickle.load(file)
         self.strategy.states = Qvalues
