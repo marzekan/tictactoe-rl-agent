7f4eec18e694ffdf645803df89284c3ddd533fcb
marzekan <marko.zekan@gmail.com>
2021-02-25 16:10:54 +0100

    Simulation settings added to simulation file

--- train.py
+++ train.py
@@ -3,15 +3,51 @@ from rules import Board
 
 from time import sleep
 
+# Simulation
+#   simulateGame
+#       opcije simulacije
+#           R - R
+#           R - Q
+#           Q - Q
+#
+#
+
 
 class Simulation:
-    def __init__(self):
+
+    def __init__(self, simulation_setting):
+
         self.board = Board()
+
+        '''
+            Simulation setting can be one of 3 states:
+                RR (Random agent vs. Random agent)
+                RQ (Random agent vs. Q-learning agent)
+                QQ (Q-learning agent vs. Q-learning agent)
+
+            States represent learning strategy of agent that
+            are being simulated.
+        '''
+
+        try:
+            simulation_setting in ["RR", "RQ", "QQ"]
+        except Exception as e:
+            print("Wrong agent behaviour passed, must be: 'RR', 'RQ' or 'QQ'.")
+            return
+
+        strategy_X = ""
+        strategy_O = ""
+
+        if simulation_setting == "RR":
+            pass
+
         self.agentX = Agent(self.board.setting, "X", strategy="q")
         self.agentO = Agent(self.board.setting, "O", strategy="q")
 
     def simulateGame(self):
+
         self.board.resetBoard()
+
         while self.board.isGameOver() is False:
 
             self.agentX.states = self.board.setting
